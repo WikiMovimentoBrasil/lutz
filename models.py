9f40d5b0f57d2d3cd2db0862e34fef52c01acbe4
Chico Venancio <chicocvenancio@wmnobrasil.org>
2022-12-16 22:06:21 -0300

    Add initial snapshot model

---
+++ models.py
@@ -0,0 +1,31 @@
+import enum
+
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import Enum
+from sqlalchemy import DateTime
+from sqlalchemy.orm import declarative_base
+
+
+class SnapshotEnum(enum.Enum):
+    recent = 'recent'
+    historical = 'historical'
+
+
+Base = declarative_base()
+
+
+class Snapshot(Base):
+    __tablename__ = "snapshot"
+
+    id = Column(Integer, primary_key=True)
+    wiki = Column(String(10))
+    type = Column(Enum(SnapshotEnum))
+    timestamp = Column(DateTime)
+    editors_male = Column(Integer)
+    editors_female = Column(Integer)
+    editors_neutral = Column(Integer)
+    edits_male = Column(Integer)
+    edits_female = Column(Integer)
+    edtis_neutral = Column(Integer)
